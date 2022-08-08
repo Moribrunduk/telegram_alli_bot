ab75e522f41991048b46bdf2cbadebf539ad2f99
Moribrunduk <Aild19921128@gamil.com>
2022-08-08 20:03:13 +0300

    List of commands

--- ali_dollar.py
+++ ali_dollar.py
@@ -5,9 +5,12 @@ name =""
 sername = ""
 age = 0
 
+Commands  = {"Курс": "позволяет увидеть курс валюты "}
+
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+	for key,value in Commands.items():
+		bot.send_message(message.from_user.id,'__'+key+'__' + " \- " + value, parse_mode='MarkdownV2')
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
@@ -30,14 +33,14 @@ def reg_sername(message):
 	bot.send_message(message.from_user.id, 'Сколько тебе лет')
 	bot.register_next_step_handler(message, reg_age)
 def reg_age(message):
+	pass
 	global age
-	while age == 0:
-		try:
-			age = int(message.text)
-			bot.send_message(message.from_user.id, 'збс')
-		except TypeError:
-			bot.send_message(message.from_user.id, 'Пиши циферами')
-			age = 0
-			bot.register_next_step_handler(message, reg_age)
+	age = 0
+	if age == int:
+		bot.send_message(message.from_user.id, 'збс')
+	else:
+		bot.send_message(message.from_user.id, 'Пиши циферами')
+		reg_age(message)
+
 
 bot.infinity_polling()
